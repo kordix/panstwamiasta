94a4a27bbb32e1c3d569ff1eade3627ecaa6372a
Kordian Bober <BOBKOR@ad.bertrand.pl>
2024-12-07 22:29:51 +0100

    3000

--- server.js
+++ server.js
@@ -90,8 +90,8 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(3002, () => {
-    console.log('listening on *:3001');
+server.listen(3000, () => {
+    console.log('listening on *:3000');
 });
 
 
